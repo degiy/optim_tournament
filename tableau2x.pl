837e3312edc7ddfcbfccca35f4ab00152a8fb51f
degiy <degiy@yahoo.fr>
2025-05-12 01:38:08 +0200

    add a -s (start) option to tableau2x.pl and courts line handling

--- tableau2x.pl
+++ tableau2x.pl
@@ -12,12 +12,34 @@ $m_duration=20;
 # format of exchange file : slot_time; court ; category ; pool ; phase (if any); team 1; team 2; score t1; score t2;
 
 # tableau format is : slot_ndx; team1 - team2 (pool); team1 - teams2 (pool/pĥase);.... as many as courts availlage/busy;
+while ($#ARGV>=0)
+{
+    if ($ARGV[0] eq '-s')
+    {
+        shift @ARGV;
+        $st=shift @ARGV;
+        ($hh,$mm)=split /:/,$st;
+        $mins=$hh*60+$mm;
+    }
+    $fname=shift @ARGV;
+}
 
-while(<>)
+$i=0;
+open F,$fname;
+while(<F>)
 {
     chomp $_;
     @l=split /;/,$_;
     $slndx=shift @l;
+    if (($i==0)&&($slndx eq ''))
+    {
+        # courts names provided, so use them
+        @court_names=();
+        for $c (@l)
+        {
+            push @court_names,$c unless $c eq '';
+        }
+    }
     next unless $slndx=~/^[0-9]/;
     #print $l[0],"\n";
     $slot=&mins2string($mins);
@@ -40,7 +62,9 @@ while(<>)
         print "$slot;",$court_names[$i],";$cat;$pool;$phase;$t1;$t2;;;\n"
     }
     $mins+=$m_duration;
+    $i++;
 }
+close F;
 
 # 620 to => 10:20
 sub mins2string
